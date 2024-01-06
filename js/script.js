da995629010681709800cb1da95488995f1bfb84
chilian_s <sierrachilian@gmail.com>
2024-01-05 22:02:51 -0500

    Created array and loop for objects

--- js/script.js
+++ js/script.js
@@ -34,8 +34,34 @@ const francine = createPet("Francine", "turtle");
 
 //console.log(sora, clover, baxter, cleo, francine);
 
-clover.sleep();
-baxter.play();
+//clover.sleep();
+//baxter.play();
 
-console.log(clover);
-console.log(baxter);
+//console.log(clover);
+//console.log(baxter);
+
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [sora, clover, baxter, cleo, francine];
+
+//console.log(allPets);
+
+const showPets = function (petArray) {
+
+    pets.innerHTML = "";
+
+    for (let pet of petArray) {
+        let status = "Ready to play!";
+        if (pet.isTired >= 7) {
+            status = "sleeping. Zzzzz";
+        }
+        const li = document.createElement("li");
+        li.innerHTML = `<span class="pet-name"> ${pet.name}</span> the ${pet.species} is ${status}`;
+        pets.append(li);
+    }
+};
+
+statusButton.addEventListener("click", function() {
+    showPets(allPets);
+});
